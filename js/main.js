d6b17bc4427830eb348fd3bc0dc5e6415cb1e1ba
kgonz002 <kgonz002@odu.edu>
2022-12-18 20:46:35 -0500

    embedded header/footer; hero image; navbar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,47 @@
-$(document).ready(function () {
-  $("#header").load("header.html");
-  $("#footer").load("footer.html");
+console.log("hi there");
+
+//Header and footer
+class CustomHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <ul class="nav-links">
+          <span class="image-clickable">
+            <li>
+              <a href="index.html">
+                <img     
+                  src="assets/images/logo.png"
+                  alt="logo"
+                  class="logo"
+                />
+              </a>
+            </li>
+          </span>
+          <li><a href="about.html">about</a></li>
+          <li><a href="shop.html">shop</a></li>
+          <li><a href="list.html">my list</a></li>
+        </ul>
+      </header>`;
+  }
+}
+
+customElements.define("cust-header", CustomHeader);
+
+class CustomFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <footer>
+        <p>© Kimberly R. Gonzales 2022</p>
+      </footer>
+    `;
+  }
+}
+
+customElements.define("cust-footer", CustomFooter);
+
+//locomotive-scroll
+
+const scroller = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
 });
